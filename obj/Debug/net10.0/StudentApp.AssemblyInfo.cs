67680c3170b91371f36461d9f5910ff639bd3929
CerinSaji <cerins2003@gmail.com>
2026-03-25 14:37:36 +0530

    Add .gitignore

--- obj/Debug/net10.0/StudentApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/StudentApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5634b8f0627a9d1fee2b0471665b89421a95d9f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8471682c5bb33eb3eade84129b15ab16b183a1ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
